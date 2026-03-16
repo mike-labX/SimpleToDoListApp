5e9a7f70c7b0320289b2069dec5ff93d943932cd
mike-labX <michaelli.workspace@gmail.com>
2026-03-16 16:35:32 -0400

    Add project files.

---
+++ SimpleToDoListApp/Controllers/TodoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleToDoListApp.Models;
+
+namespace SimpleToDoListApp.Controllers
+{
+    public class TodoController : Controller
+    {
+        private static List<TodoItem> todos = new List<TodoItem>();
+        public IActionResult Index()
+        {
+            return View(todos);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string title)
+        {
+            if (!string.IsNullOrEmpty(title))
+            {
+                todos.Add(new TodoItem { Id = todos.Count + 1, Title = title, IsDone = false });
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Toggle(int id)
+        {
+            var item = todos.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                item.IsDone = !item.IsDone;
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SimpleToDoListApp/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.Eventing.Reader;
+
+namespace SimpleToDoListApp.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+
+        public bool IsDone { get; set; }
+    }
+}
